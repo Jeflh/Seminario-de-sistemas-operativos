3eaf1cc0be3bfe819ee4662ad6464df1b2ec74a8
Emmanuel Fernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2023-02-03 16:52:32 -0600

    Almacenamiento de datos completo

---
+++ Parte 1/main.py
@@ -0,0 +1,38 @@
+
+
+MAX_CAPACITY = 4 # Procesos máximos por lote
+
+def newProcess():
+  name = input('\nNombre del programador: ')
+  operation = input('Operación a realizar: ')
+  maxTime = input('Tiempo máximo estimado: ')
+  numberID = input('Número de programa (ID): ')
+
+  # Validación de datos 
+  if name == '' or operation == '' or maxTime == '' or numberID == '':
+    print('No se puede agregar el proceso, por favor ingrese todos los datos')
+    return None
+
+  return [name, operation, maxTime, numberID]
+
+
+if __name__ == '__main__':
+  # Código principal
+  lots = []
+  batch = []
+
+  processes = int(input('Ingrese la cantidad de procesos que desea agregar: '))
+
+  while processes != 0:
+    process = newProcess()
+
+    if process is not None:
+      batch.append(process)
+
+    if len(batch) == MAX_CAPACITY or processes == 1:
+      lots.append(batch)
+      batch = []
+
+    processes -= 1
+
+  print(lots)
