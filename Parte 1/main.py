032eeb5e2053b54d3b6c30226d22310134ca9e36
Emmanuel Fernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2023-02-03 19:23:10 -0600

    Validación de tiempo y operación

--- Parte 1/main.py
+++ Parte 1/main.py
@@ -2,7 +2,9 @@
 
 MAX_CAPACITY = 4 # Procesos máximos por lote
 
-def newProcess():
+
+def newProcess(processes):
+  print(f'\nProceso {processes}')
   name = input('\nNombre del programador: ')
   operation = input('Operación a realizar: ')
   maxTime = input('Tiempo máximo estimado: ')
@@ -10,10 +12,32 @@ def newProcess():
 
   # Validación de datos 
   if name == '' or operation == '' or maxTime == '' or numberID == '':
-    print('No se puede agregar el proceso, por favor ingrese todos los datos')
+    print('\nNo se puede dejar ningún campo vacío.')
+    return None
+
+  if not validTime(maxTime):
+    print('\nEl tiempo máximo debe ser un número entero mayor a 0.')
+    return None
+
+  if not validateOperation(operation):
+    print('\nLa operación ingresada no es válida.')
     return None
 
   return [name, operation, maxTime, numberID]
+
+
+def validOperation (operation):
+  if '+' in operation or '-' in operation or '*' in operation or '/' in operation or '%' in operation:
+    return True
+  else:
+    return False
+
+
+def validTime (maxTime):
+  if maxTime.isdigit() and int(maxTime) > 0:
+    return True
+  else:
+    return False
 
 
 if __name__ == '__main__':
@@ -24,15 +48,15 @@ if __name__ == '__main__':
   processes = int(input('Ingrese la cantidad de procesos que desea agregar: '))
 
   while processes != 0:
-    process = newProcess()
+    process = newProcess(processes)
 
     if process is not None:
       batch.append(process)
+      
+      if len(batch) == MAX_CAPACITY or processes == 1:
+        lots.append(batch)
+        batch = []
 
-    if len(batch) == MAX_CAPACITY or processes == 1:
-      lots.append(batch)
-      batch = []
-
-    processes -= 1
-
+      processes -= 1
+    
   print(lots)
