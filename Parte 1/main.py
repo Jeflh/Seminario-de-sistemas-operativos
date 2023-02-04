a0981a70ee511d43f9d9cc1cdbb85b59b9b67c30
JuanSalvi <juan.salazar1291@alumnos.udg.mx>
2023-02-03 19:24:41 -0600

    Validacion ID

--- Parte 1/main.py
+++ Parte 1/main.py
@@ -1,21 +1,37 @@
 
 
 MAX_CAPACITY = 4 # Procesos máximos por lote
+registeredId = []
 
 def newProcess():
   name = input('\nNombre del programador: ')
   operation = input('Operación a realizar: ')
   maxTime = input('Tiempo máximo estimado: ')
   numberID = input('Número de programa (ID): ')
-
+  
+  
   # Validación de datos 
+  if not validId(numberID):
+    print('El ID ya se encuentra registrado, por favor ingrese otro')
+    return None
+
   if name == '' or operation == '' or maxTime == '' or numberID == '':
     print('No se puede agregar el proceso, por favor ingrese todos los datos')
     return None
 
+  registeredId.append(numberID) # Agrega el ID a la lista de IDs registrados
+  print(registeredId) # Imprime la lista de IDs registrados
   return [name, operation, maxTime, numberID]
 
+# Validación de ID
+def validId(numberID):
+  
+  if numberID in registeredId:
+    return False
+  else:
+    return True
 
+    
 if __name__ == '__main__':
   # Código principal
   lots = []
@@ -36,3 +52,8 @@ if __name__ == '__main__':
     processes -= 1
 
   print(lots)
+
+
+
+git config --global user.email "juan.salazar1291@alumnos.udg.mx"
+git config --global user.name "JuanSalvi"
